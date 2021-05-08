cdbef944734fa77e6130fa681b41a9ed490c195e
Bahtiyor Sultanov <baxt5878@gmail.com>
2021-05-08 15:50:59 +0200

    addItemModal added

--- client/layout.js
+++ client/layout.js
@@ -1,4 +1,30 @@
-Template.Layout.onRendered(function(){
-    $('.sidenav').sidenav();
+Template.Layout.onCreated(function(){
 });
+
+Template.Layout.onRendered(function(){
+});
+
+Template.Layout.events({
+    'change select#language':function (event) {
+        event.preventDefault();
+        TAPi18n.setLanguage(event.target.value);
+    },
+    'click button#addItem': function (event) {
+        event.preventDefault();
+        $('.modal#addItemModal').addClass('is-active');
+        $('html').addClass('is-clipped');
+        
+    },
+    'click .modal-close': function (event) {
+        event.preventDefault();
+        $('.modal').removeClass('is-active');
+        $('html').removeClass('is-clipped');
+
+    }
+});
+
+Template.Layout.helpers({
+    
+});
+
 
