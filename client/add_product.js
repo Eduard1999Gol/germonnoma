f0cc667f63f57dfbb8b847c1540b35a28d91f4dd
Eduard1999Gol <edgol199@gmail.com>
2021-05-15 13:22:48 +0200

    Fehler

---
+++ client/add_product.js
@@ -0,0 +1,55 @@
+import { toast } from 'bulma-toast';
+import helper_functions from './lib/helper_functions';
+
+Template.AddProduct.onCreated(function(){
+});
+
+Template.AddProduct.onRendered(function(){
+});
+
+Template.AddProduct.events({
+    'click button#addProduct': function (event) {
+        event.preventDefault();
+        $('.modal#addProductModal').addClass('is-active');
+        $('html').addClass('is-clipped');
+        
+    },
+    'click .close-modal': function (event) {
+        event.preventDefault();
+        helper_functions.closeModal();
+    },
+    'submit form#addProductForm':function (event) {
+        event.preventDefault();
+        var product = {
+            name: event.target.product_name.value,
+            price: event.target.product_price.value,
+            description: event.target.product_description.value
+        }
+        Meteor.call('createProduct', product, function (err, res) {
+            if (!err) {
+                toast({
+                    message: TAPi18n.__('product_created'),
+                    type: 'is-success',
+                    duration: 350,
+                    position: "bottom-right",
+                    closeOnClick: true
+                });
+            }else{
+                toast({
+                    message: TAPi18n.__('product_not_created'),
+                    type: 'is-danger',
+                    duration: 350,
+                    position: "bottom-right",
+                    closeOnClick: true
+                });
+            }
+        });
+        helper_functions.closeModal();
+    }
+});
+
+Template.AddProduct.helpers({
+    
+});
+
+
--- client/layout.js
+++ client/layout.js
@@ -12,44 +12,6 @@ Template.Layout.events({
         event.preventDefault();
         TAPi18n.setLanguage(event.target.value);
     },
-    'click button#addProduct': function (event) {
-        event.preventDefault();
-        $('.modal#addProductModal').addClass('is-active');
-        $('html').addClass('is-clipped');
-        
-    },
-    'click .close-modal': function (event) {
-        event.preventDefault();
-        helper_functions.closeModal();
-    },
-    'submit form#addProductForm':function (event) {
-        event.preventDefault();
-        var product = {
-            name: event.target.product_name.value,
-            price: event.target.product_price.value,
-            description: event.target.product_description.value
-        }
-        Meteor.call('createProduct', product, function (err, res) {
-            if (!err) {
-                toast({
-                    message: TAPi18n.__('product_created'),
-                    type: 'is-success',
-                    duration: 350,
-                    position: "bottom-right",
-                    closeOnClick: true
-                });
-            }else{
-                toast({
-                    message: TAPi18n.__('product_not_created'),
-                    type: 'is-danger',
-                    duration: 350,
-                    position: "bottom-right",
-                    closeOnClick: true
-                });
-            }
-        });
-        helper_functions.closeModal();
-    }
 });
 
 Template.Layout.helpers({
