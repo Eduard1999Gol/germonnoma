0cfc51c2be38bbbedd621a8b7eb4b550379b595d
Bahtiyor Sultanov <baxt5878@gmail.com>
2021-06-01 23:50:25 +0200

    create product upload image changed

--- client/add_product.js
+++ client/add_product.js
@@ -3,6 +3,7 @@ import helper_functions from './lib/helper_functions';
 import category from './product_category.js';
 
 Template.AddProduct.onCreated(function(){
+    Session.set('selectedFile', "");
 });
 
 Template.AddProduct.onRendered(function(){
@@ -23,9 +24,26 @@ Template.AddProduct.events({
             price: event.target.product_price.value,
             description: event.target.product_description.value
         }
-        console.log(product)
+        console.log(event);
         Meteor.call('createProduct', product, function (err, res) {
             if (!err) {
+                console.log(res); //erstmal erstellen wir product und von Methode als res bekommen wir erstellte product id
+                /* wenn ich id habe dann kann ich direkt meine Image in ProductImage collection hochladen und danach toast zeigen */
+                const upload = ProductImages.insert({
+                    file: event.target.resume.files[0],
+                    chunkSize: 'dynamic',
+                    meta: {
+                        product_id: res //hier benutze ich product id
+                    }
+                    }, false);
+                    upload.on('end', function (error, fileObj) {
+                    if (error) {
+                        return error;
+                    } else {
+                        return true;
+                    }
+                    });
+                    upload.start();
                 toast({
                     message: TAPi18n.__('product_created'),
                     type: 'is-success',
@@ -44,38 +62,22 @@ Template.AddProduct.events({
                 });
             }
         });
-        helper_functions.closeModal();
     },
     'change input.file-input':function (event) {
         event.preventDefault();
-        var id = Template.instance().data.product._id;
-        Meteor.call('createImage', id, function(err, res){
-            if(!err){
-                const upload = ProductImages.insert({
-                    file: event.target.files[0],
-                    chunkSize: 'dynamic',
-                    meta: {
-                        product_id: id
-                    }
-                  }, false);
-                  upload.on('end', function (error, fileObj) {
-                    if (error) {
-                      return error;
-                    } else {
-                      return true;
-                    }
-                  });
-                  upload.start();
-            }else{
-                console.log(err);
-            }
-        })
+        Session.set('selectedFile', event.target.files[0].name);
     },
 });
 
 
 Template.AddProduct.helpers({
-  
+  'selectedFile':function () {
+      if (Session.get('selectedFile')) {
+          return Session.get('selectedFile');
+      } else {
+        return "choose_picture";
+      }
+  }
         
 });
 
