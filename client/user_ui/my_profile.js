f3c454e9fe9d4e507bd9bdf1e70b06027c7f31a4
Eduard Gol <edgol199@gmail.com>
2021-08-18 22:35:00 +0200

    select language on profile and register page added

--- client/user_ui/my_profile.js
+++ client/user_ui/my_profile.js
@@ -1,6 +1,4 @@
 Template.MyProfile.onRendered(function () {
-    console.log(document.getElementById("selected_language").innerText[0]);
-    //TAPi18n.setLanguage(Template.instance().data.user.profile.language);
 })
 
 
