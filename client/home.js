b2a39952f55694bb24b32e11c667c572291b96cd
Eduard Gol <edgol199@gmail.com>
2021-09-30 15:51:08 +0200

    search field done

--- client/home.js
+++ client/home.js
@@ -1,8 +1,8 @@
 Template.Home.events({
     'submit form.searchForm': function (event) {
         event.preventDefault();
-        console.log(event.target.search.value)
-        var key = event.currentTarget.search.value;
-        Session.set('searchTerm',key);
+        if (event.currentTarget.search.value) {
+            Router.go("searchedProducts", {searchTerm: event.currentTarget.search.value});
+        }  
     }
 })
